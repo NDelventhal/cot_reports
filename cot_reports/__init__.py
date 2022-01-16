edca4b5c2409638d45474656129c89f5f2f9511b
NDelventhal <56277004+NDelventhal@users.noreply.github.com>
2022-01-16 15:08:39 +0100

    version update '0.1.1'

---
+++ cot_reports/__init__.py
@@ -0,0 +1,4 @@
+from .cot_reports import cot_hist, cot_year, cot_all, cot_all_reports
+
+__version__ = '0.1.1'
+__author__ = 'Niall Delventhal'
--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ import setuptools
 setuptools.setup(
     name="cot_reports",
     packages = ["cot_reports"], 
-    version="0.1.0",
+    version="0.1.1",
     license='MIT',  
     url="https://github.com/NDelventhal/cot_grades",
     author="Niall Delventhal",
@@ -11,7 +11,7 @@ setuptools.setup(
     description="is a Python library, which fetches the Commitments of Trader reports of the Commodity Futures Trading Commission (CFTC). The following COT reports are supported: Legacy Futures-only, Legacy Futures-and-Options, Supplemental Futures-and-Options, Disaggregated Futures-only, Disaggregated Futures-and-Options, Traders in Financial Futures (TFF) Futures-only and Traders in Financial Futures (TFF) Futures-and-Options.",
     long_description=open('README.md').read(),
     long_description_content_type='text/markdown',
-    download_url = 'https://github.com/NDelventhal/cot_reports/archive/v_010.tar.gz',
+    download_url = 'https://github.com/NDelventhal/cot_reports/archive/v_011.tar.gz',
     install_requires=['pandas','requests','beautifulsoup4'],
     classifiers=['Intended Audience :: Science/Research', 
     'License :: OSI Approved :: MIT License', 
